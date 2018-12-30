baf8ecb606ae9e6a107ce7d7a75be0e2d5a7b443
Bpedley <v3rtex12@gmail.com>
2018-12-30 20:15:40 +0300

    Small fixes

--- Other projects/Calculator/app.js
+++ Other projects/Calculator/app.js
@@ -1,68 +1,114 @@
 let keys = document.querySelector(".keys");
 let calcValue = document.querySelector(".calc-value");
 let fullExp = document.querySelector("#exp");
-let lastPart, operator, currentPart;
+let exp, regex;
 
 
+// add click event on all calculator buttons using event delegation
 keys.onclick = function(e) {
-  var target = e.target;
+  let target = e.target;
 
+  // if clear button clicked
   if (target.classList.contains("all-clear")) {
+    // clear value in calcValue
     calcValue.value = 0;
+    // remove exp value if exists
+    if (exp) {
+      exp = null;
+    }
   }
 
+  // if number button clicked
   if (target.classList.contains("number")) {
+    // call function to display value of clicked button
     highlightNumber(target);
+    // remove exp value if exists, remove value in calcValue
+    if (exp) {
+      exp = null;
+      calcValue.value = 0;
+      highlightNumber(target);
+    }
   }
 
+  // if decimal button clicked
   if (target.classList.contains("decimal")) {
+    // call function to add decimal point
     addDecimalPoint(target);
   }
 
+  // if operator button clicked
   if (target.classList.contains("operator")) {
+    // call function to add operator
     addOperator(target);
   }
 
+  // if equal button clicked
   if (target.classList.contains("equal")) {
+    // call function to calculate result
     calculateValue();
   }
 };
 
 
 function highlightNumber(node) {
-  if (calcValue.value == 0 && String(calcValue.value).length == 1) {
-    calcValue.value = node.value;
-  } else if(String(calcValue.value).length >= 15) {
+  // using regex, split calcValue into array and store last item
+  let regex = String(calcValue.value).split(/[\-\+\*\/]/).slice(-1);
+  /*
+  if:
+  -calculated value exists
+  -regex first letter and button value 0
+  -calcValue length bigger or equal 17
+  */
+  if (exp || (regex.slice(0, 1) == "0" && node.value == 0) || (String(calcValue.value).length >= 17)) {
+    // do nothing
     return;
+  // if calcValue = 0 and length = 1
+  } else if (calcValue.value == 0 && String(calcValue.value).length == 1) {
+    // calcValue = clicked button value
+    calcValue.value = node.value;
   } else {
+    // add to calcValue clicked button value
     calcValue.value += node.value;
   }
 }
 
 
 function addDecimalPoint(node) {
-  let regex = String(calcValue.value).split(/[\-\+\*\/]/).slice(-1)[0];
-  
-  if (regex.includes(".")) return;
-
+  // using regex, split calcValue into array and store first letter of last item
+  regex = String(calcValue.value).split(/[\-\+\*\/]/).slice(-1)[0];
+  // if regex have dot, empty string or exp value exists - do nothing
+  if (regex.includes(".") || regex == "" || exp) return;
+  // add to calcValue decimal point
   calcValue.value += node.value;
 }
 
 
 function calculateValue() {
-  let exp = calcValue.value;
-
-  if (exp) {
-    calcValue.value = eval(exp);
+  // using regex, split calcValue into array and store last item
+  regex = String(calcValue.value).split(/[\-\+\*\/]/).slice(-1);
+  // if 2 value is empty - don't calculate
+  if (regex != "") {
+    exp = calcValue.value;
+    // show only 17 letters result
+    calcValue.value = String(eval(exp)).slice(0, 17);
   }
 }
 
 
 function addOperator(node) {
-  let res = String(calcValue.value);
-
-  if ("+-/*".indexOf(res.charAt(res.length - 1)) >= 0) {
-    calcValue.value = res.slice(0, -1) + node.value;
+  // store result of calcValue field
+  let result = String(calcValue.value);
+  // if exp value exists and result length above 17
+  if (exp && result.length < 17) {
+    exp = null;
+    calcValue.value += node.value;
+  // if result length bigger or equal 17 - do nothing
+  } else if (result.length >= 17) {
+    return;
+  // if last letter in calcValue operator - change to different if clicked not the same
+  } else if ("+-/*".indexOf(result.charAt(result.length - 1)) >= 0) {
+    calcValue.value = result.slice(0, -1) + node.value;
+  // add operator
   } else {
     calcValue.value += node.value;
   }
