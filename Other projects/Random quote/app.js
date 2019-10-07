64264dd1de46b33554371d5ead07e704ed4682e4
Bpedley <v3rtex12@gmail.com>
2019-10-07 12:01:35 +0300

    Update app.js
    Changed request method

--- Other projects/Random quote/app.js
+++ Other projects/Random quote/app.js
@@ -1,31 +1,47 @@
 "use strict";
 
-const url = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
-const colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
-let quoteDiv = document.querySelector(".quote");
-let quoteText = document.querySelector("#quote-text");
-let authorName = document.querySelector("#quote-author");
-let newQuoteButton = document.querySelector("#new-quote");
-let twitterButton = document.querySelector("#twitter");
-let data;
+const url =
+  "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
+const colors = [
+  "#16a085",
+  "#27ae60",
+  "#2c3e50",
+  "#f39c12",
+  "#e74c3c",
+  "#9b59b6",
+  "#FB6964",
+  "#342224",
+  "#472E32",
+  "#BDBB99",
+  "#77B1A9",
+  "#73A857"
+];
+const quoteDiv = document.querySelector(".quote");
+const quoteText = document.querySelector("#quote-text");
+const authorName = document.querySelector("#quote-author");
+const newQuoteButton = document.querySelector("#new-quote");
+const twitterButton = document.querySelector("#twitter");
+const tweetUrl = "https://twitter.com/intent/tweet?hashtags=quotes&text=";
+let data, quote, color;
 
-
-// get random quote from json object
+// get random quote
 function randomQuote() {
-  return [data.quotes[Math.floor((Math.random() * (data.quotes.length)))].quote,
-          data.quotes[Math.floor((Math.random() * (data.quotes.length)))].author];
+  return [
+    data[Math.floor(Math.random() * data.length)].quote,
+    data[Math.floor(Math.random() * data.length)].author
+  ];
 }
 
 // get random color from array
 function randomColor() {
-  return colors[Math.floor((Math.random() * (colors.length)))];
+  return colors[Math.floor(Math.random() * colors.length)];
 }
 
 // set content of quote div
 function setContent() {
   // call functions and store result
-  let quote = randomQuote();
-  let color = randomColor();
+  quote = randomQuote();
+  color = randomColor();
   // set quote and author
   quoteText.textContent = quote[0];
   authorName.textContent = "- " + quote[1];
@@ -41,36 +57,22 @@ function setContent() {
   newQuoteButton.style.backgroundColor = color;
 }
 
-// get data using http request
-function getQuotes(url, callback) {
-  // Create a request variable and assign a new XMLHttpRequest object to it.
-  let request = new XMLHttpRequest();
-  // Open a new connection, using the GET request on the URL endpoint
-  request.open('GET', url, true);
-  request.onload = () => {
-    if (request.status >= 200 && request.status < 400) {
-      data = JSON.parse(request.responseText);
-      // call setContent function
-      callback();
-    } else {
-      // if error
-      console.log("Error happened");
-    }
-  }
-  // send request
-  request.send();
+// get data using fetch
+function getQuotes(url) {
+  return fetch(url).then(response => {
+    response.json().then(json => {
+      data = json.quotes;
+      setContent();
+    });
+  });
 }
 
 // set the link to tweet button
 function tweet(quote, author) {
-  let tweetUrl = "https://twitter.com/intent/tweet?hashtags=quotes&text=";
   return tweetUrl + encodeURIComponent(`"${quote}" ${author}`);
 }
 
 // Change content of quote div on button click
 newQuoteButton.addEventListener("click", setContent);
 
-// page loads
-document.addEventListener("DOMContentLoaded", () => {
-  getQuotes(url, setContent);
-});
+getQuotes(url);
