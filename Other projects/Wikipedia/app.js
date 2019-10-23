e483e86bbb320a21d312559962994756f32d0912
Bpedley <v3rtex12@gmail.com>
2019-10-23 12:24:30 +0300

    Changed request method

--- Other projects/Wikipedia/app.js
+++ Other projects/Wikipedia/app.js
@@ -1,14 +1,15 @@
 "use strict";
 
-let wikiApi = "https://ru.wikipedia.org/w/api.php?action=opensearch&origin=*&search=";
+let wikiApi =
+  "https://ru.wikipedia.org/w/api.php?action=opensearch&origin=*&search=";
 let searchBar = document.querySelector("#article-link");
 let searchResultDiv = document.querySelector(".search-result");
 let hint = document.querySelector("#hint");
+let url, searchError, article, titleName, about;
 
 // search wikipedia articles when 'enter' button pressed
-searchBar.addEventListener("keypress", (e) => {
-  var key = e.which || e.keyCode;
-  
+searchBar.addEventListener("keypress", e => {
+  const key = e.which || e.keyCode;
   if (key === 13) {
     // if div with articles already in document, then delete all data
     if (searchResultDiv.childNodes) {
@@ -22,68 +23,61 @@ searchBar.addEventListener("keypress", (e) => {
     // call function and get data
     getData();
   }
-})
+});
 
 // get data from api and creates articles elements
-function getData() {
-  let search = wikiApi + searchBar.value;
-  let request = new XMLHttpRequest();
+const getData = () => {
+  url = wikiApi + searchBar.value;
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      setContent(data);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
 
-  request.open('GET', search, true);
-
-  request.onload = function () {
-    // Begin accessing JSON data here
-    let data = JSON.parse(this.response);
-    // if no errors
-    if (request.status >= 200 && request.status < 400) {
-      hint.hidden = true;
-      // if result dont have data
-      if (!data[1].length) {
-        let searchError = document.createElement("div");
-        searchError.className = "search-error";
-        searchError.textContent = `Не могу найти статьи всключающие ${searchBar.value}, повторите поиск.`;
-        searchResultDiv.appendChild(searchError);
-        return;
+const setContent = data => {
+  if (!data[1].length) {
+    searchError = document.createElement("div");
+    searchError.className = "search-error";
+    searchError.className = "search-error";
+    searchError.textContent = `Не могу найти статьи всключающие ${searchBar.value}, повторите поиск.`;
+    searchResultDiv.appendChild(searchError);
+    return;
+  } else {
+    for (let i = 0; i < data[1].length; i++) {
+      article = document.createElement("div");
+      article.className = "article";
+      article.setAttribute("data-link", data[3][i]);
+      titleName = document.createElement("h3");
+      titleName.innerHTML = data[1][i];
+      about = document.createElement("p");
+      if (data[2][i] == "") {
+        about.innerHTML = "Без описания";
+      } else {
+        about.innerHTML = data[2][i];
       }
-
-      for (let i = 0; i < data[1].length; i++) {
-        let article = document.createElement("div");
-        article.className = "article";
-        article.setAttribute("data-link", data[3][i]);
-
-        let titleName = document.createElement("h3");
-        titleName.innerHTML = data[1][i];
-
-        let about = document.createElement("p");
-        if (data[2][i] == "") {
-          about.innerHTML = "Без описания";
-        } else {
-          about.innerHTML = data[2][i];
-        }
-
-        searchResultDiv.appendChild(article);
-        article.appendChild(titleName);
-        article.appendChild(about);
-      }
-    } else {
-      // if error happened
-      console.log("Error happened");
+      appendElements();
     }
   }
-  // send request
-  request.send();
-}
+};
+
+const appendElements = () => {
+  searchResultDiv.appendChild(article);
+  article.appendChild(titleName);
+  article.appendChild(about);
+};
 
 // opens article link on other tab
-searchResultDiv.onclick = function(e) {
+searchResultDiv.onclick = e => {
   var target = e.target;
-
   while (target != searchResultDiv) {
-    if (target.className == 'article') {
+    if (target.className == "article") {
       window.open(target.getAttribute("data-link"));
       return;
     }
-
     target = target.parentNode;
   }
-}
+};
